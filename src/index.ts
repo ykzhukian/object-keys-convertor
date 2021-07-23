b5d130583c261f4c37d9fd8eeca15fa05f4d3860
祝云开 <zhuyunkai@baixing.com>
2021-07-23 11:42:41 +0800

    add convertor string

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Mapper from './Mapper';
+import Convertor from './Convertor';
 
 export const toCamelcase = (obj: Obj): Obj => {
   if (typeof obj !== 'object') return obj;
@@ -10,4 +11,16 @@ export const toSnakecase = (obj: Obj): Obj => {
   if (typeof obj !== 'object') return obj;
   const mapper = new Mapper(obj);
   return mapper.toSnakecase();
+};
+
+export const toCamelcaseStr = (obj: string): string => {
+  if (typeof obj !== 'string') return obj;
+  const c = new Convertor(obj);
+  return c.toCamelcase();
+};
+
+export const toSnakecaseStr = (obj: string): string => {
+  if (typeof obj !== 'string') return obj;
+  const c = new Convertor(obj);
+  return c.toSnakecase();
 };
